afb1dd26cb2e39c07c5bbd75a586a09af4751e6b
uilsonpassos <86895106+uilsonpassos@users.noreply.github.com>
2021-09-09 01:26:32 -0300

    modificando

--- at04/q06/ler3letras.c
+++ at04/q06/ler3letras.c
@@ -2,32 +2,22 @@
 #include <string.h>
 #include <stdlib.h>
 
-void ler3letras(char primeira[], char segunda[], char terceira[]);
+void ler3letras(char leitor[]);
 
 int main()
 { 
-    char ler1val[20];
-    char ler2val[20];
-    char ler3val[20];
-    ler3letras(ler1val, ler2val, ler3val);
+    char ler3val[4];
+    ler3letras(ler3val);
     
-        printf("A primeira palavra eh: %s -- \n", ler1val);
-        printf("A segunda palavra eh: %s -- \n", ler2val);
-        printf("A terceira palavra eh: %s -- \n", ler3val);
+        printf("A sequencia eh: %s -- ", ler3val);
 
 
     return 0;
 }
 
-void ler3letras(char primeira[], char segunda[], char terceira[]){
+void ler3letras(char leitor[]){
     
 
-        printf("Digite a primeira palavra: \n");
-        fgets(primeira, 20, stdin);
-        
-        printf("Digite a segunda palavra: \n");
-        fgets(segunda, 20, stdin);
-        
-        printf("Digite a terceira palavra: \n");
-        fgets(terceira, 20, stdin);
+        printf("Digite uma letra para a sequencia: \n");
+        fgets(leitor, 4, stdin);
 }

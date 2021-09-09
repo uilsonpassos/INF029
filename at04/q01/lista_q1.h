7bc5156d23387f0c951a563525a4ab5bd5c29dbc
uilsonpassos <86895106+uilsonpassos@users.noreply.github.com>
2021-09-09 00:57:42 -0300

    Modificando

--- at04/q01/lista_q1.h
+++ at04/q01/lista_q1.h
@@ -1,16 +1,1 @@
-#include <stdio.h>
-int main(void)
-{
-	
-    int numero_a;
-    int numero_b;
-    
-    printf("Digite o numero A: \n");
-    scanf("%d", &numero_a);
-    printf("Digite o numero B: \n");
-    scanf("%d", &numero_b);
-    int Operacao = soma( numero_a, numero_b);
-    printf("O resultado eh: %d", Operacao);
-
-	return 0;
-}
+int soma(int numero_a, int numero_b);
